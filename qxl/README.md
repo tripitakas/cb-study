01015cdaf8d84a6581d62a5eac5e6591f4222ccb
Zhang Yungui <rhcad@hotmail.com>
2020-07-30 11:27:03 +0800

    合并为两栏页面

--- qxl/README.md
+++ qxl/README.md
@@ -7,6 +7,22 @@
    下载方法：点击“设定”按钮，开启与关闭系统外挂，启用“经文导出按钮”，然后“汇出”全部HTML。
    如果是多卷网页，则将其余网页中的`<div id='body'>`节点的所有文本(折叠节点后更方便复制)依次复制到第一个网页中。
 
+2. 将这两个网页合并为一个文件：一卷梁译的`<div id='body'>`改为`<div id='body-left'>`，
+   两卷唐译的第一个`<div id='body'>`改为`<div id='body-right'>`，删除其余的`</div><div id='body'>`。
+   删除`cbeta-copyright`节点。
+   
+   然后运行 `python3 util/p_add_id.py qxl/T1666-1667.html`为每个段落分配编号，得到 `T1666-1667.html`。
+   
+3. 将合并后的网页改为左右两栏的页面，加上 bootstrap、jquery、cb.css 的引用，将上面的 `<div id='body...>` 改造为：
+
+```html
+<div id="content" class="container-fluid">
+  <div class="row">
+    <div class="original col-xs-6" id='body-left'...>
+    <div class="original col-xs-6" id='body-right'...>
+  </div>
+</div>
+```
 
 [CB]: http://cbetaonline.cn
 [T1666]: http://cbetaonline.cn/zh/T1666_001
--- util/p_add_id.py
+++ util/p_add_id.py
@@ -7,15 +7,31 @@ import re
 def main(src_file, dst_file=''):
     with open(src_file) as f:
         lines = f.readlines()
-    pid = 0
+    pid = gid = 0
+    juan_head = None
     for i, text in enumerate(lines):
-        if len(re.findall('<p', text)) > 1:
-            print('Line %d has multiple <p>' % (i + 1))
-            return
-        if re.search('<p[> ]', text):
-            pid += 1
-            lines[i] = re.sub("<p( id='.+')", "<p id='p%d'" % pid, text)
-    print('%d ids added' % pid)
+        text = re.sub(r'^\s*No[.0-9 ]+(\[No[.0-9 ]+\])?<', '<', text)  # Remove `No. 1666 [No. 1667]`
+        juan_name = re.findall("class='juan'>([^<]+)<", text)
+        if juan_name:
+            if juan_head != juan_name[0]:
+                juan_head = juan_name[0]
+            else:
+                text = text.replace("'juan'", "'juan-end'")
+
+        text = text.replace('><p', '>\n<p').split('\n')
+        for j, t in enumerate(text):
+            if len(re.findall('<p', t)) > 1:
+                print('Line %d has multiple <p>' % (i + 1))
+                return
+            t = re.sub("<span class='lineInfo' line='[^']+'></span>", '', t.replace(' class=""', ''))
+            if re.search('<p[> ]', t):
+                pid += 1
+                text[j] = re.sub("<p( id='[^']+')?", "<p id='p%d'" % pid, t)
+            if re.search('lg-row', t):
+                gid += 1
+                text[j] = re.sub("class='lg-row'( id='[^']+')?", "class='lg-row' id='g%d'" % gid, t)
+        lines[i] = '\n'.join(text)
+    print('%d, %d ids added' % (pid, gid))
     with open(dst_file or src_file, 'w') as f:
         f.writelines(lines)
 
